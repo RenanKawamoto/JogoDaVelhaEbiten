380a830458dfb2d4124c701b87f3479bfa1a35e8
RenanKawamoto <renan_kawamoto@hotmail.com>
2022-12-21 16:13:45 -0300

    Iniciado o projeto jogo da velha com ebiten

---
+++ cmd/JogoDaVelha/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+
+	"github.com/RenanKawamoto/JogoDaVelhaEbiten/internal/Jogo"
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+func main() {
+	ebiten.SetWindowSize(500, 500)
+	ebiten.SetWindowTitle("Jogo da Velha")
+	if err := ebiten.RunGame(&Jogo.Game{}); err != nil {
+		log.Fatal(err)
+	}
+}
