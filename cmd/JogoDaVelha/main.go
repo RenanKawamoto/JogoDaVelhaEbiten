7fd98892354f2160e48dbd04f494029181e8737f
RenanKawamoto <renan_kawamoto@hotmail.com>
2023-01-04 10:51:37 -0300

    Finalizado alterações para deixar a tela cheia

--- cmd/JogoDaVelha/main.go
+++ cmd/JogoDaVelha/main.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	ebiten.SetWindowSize(500, 500)
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
 	ebiten.SetWindowTitle("Jogo da Velha")
 	if err := ebiten.RunGame(&Jogo.Game{}); err != nil {
 		log.Fatal(err)
